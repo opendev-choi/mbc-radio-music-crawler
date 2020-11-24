030eff630282203324f027c7daa7dcda548681e8
JongHyeok Choi <opendev.choi@gmail.com>
2020-11-24 21:35:01 +0900

    add: test skeleton

---
+++ tests/musicCrawlerTest.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class MusicCrawlerTest(TestCase):
+    def test_search_one_music_list_ids_by_date(self):
+        pass
+
+    def test_search_one_page_music_list_ids_by_date(self):
+        pass
+
+    def test_search_many_page_music_list_ids_by_date(self):
+        pass
+
+    def test_parse_music_id_list_from_page(self):
+        pass
+
+    def test_parse_music_list_from_page(self):
+        pass
