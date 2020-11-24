9b7e2ae5474814fcd5c7a6ad45c1fb6741760f25
JongHyeok Choi <opendev.choi@gmail.com>
2020-11-24 22:48:58 +0900

    feat: add test test_search_one_page_music_list_ids

--- tests/musicCrawlerTest.py
+++ tests/musicCrawlerTest.py
@@ -17,6 +17,12 @@ class MusicCrawlerTest(TestCase):
         self.assertEqual(len(result), 1)
         self.assertEqual(mocked_requests.call_count, 1)
 
+    @patch('requests.post', side_effect=[PropertyMock(text=html) for html in ONE_PAGE_MUSIC_LIST_ID_HTML])
+    def test_search_one_page_music_list_ids(self, mocked_requests):
+        result = self.music_crawler._search_music_list_ids_by_date(date.today(), ProgramCode.MUSIC_CAMP)
+
+        self.assertEqual(len(result), 10)
+        self.assertEqual(mocked_requests.call_count, 2)
     def test_parse_music_id_list_from_page(self):
         pass
 
