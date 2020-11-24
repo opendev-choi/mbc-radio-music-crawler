c2c04abd9170cea82e68360e1e71363e13aababc
JongHyeok Choi <opendev.choi@gmail.com>
2020-11-24 22:48:47 +0900

    feat: add test test_search_one_music_list_ids

---
+++ tests/constants.py
@@ -0,0 +1,75 @@
+ONE_MUSIC_LIST_ID_HTML = """<div id="musicWrap">
+    <table>
+        <tbody>
+            <td><a href="page?seqID=1">test-1</a></td>
+        </tbody>
+    </table>
+</div>"""
+
+ONE_PAGE_MUSIC_LIST_ID_HTML = ["""<div id="musicWrap">
+    <table>
+        <tbody>
+            <td><a href="page?seqID=1">test-1</a></td>
+            <td><a href="page?seqID=2">test-2</a></td>
+            <td><a href="page?seqID=3">test-3</a></td>
+            <td><a href="page?seqID=4">test-4</a></td>
+            <td><a href="page?seqID=5">test-5</a></td>
+            <td><a href="page?seqID=6">test-6</a></td>
+            <td><a href="page?seqID=7">test-7</a></td>
+            <td><a href="page?seqID=8">test-8</a></td>
+            <td><a href="page?seqID=9">test-9</a></td>
+            <td><a href="page?seqID=10">test-10</a></td>
+        </tbody>
+    </table>
+</div>""", """<div id="musicWrap">
+    <table>
+        <tbody>
+        </tbody>
+    </table>
+</div>"""]
+
+MANY_PAGE_MUSIC_LIST_ID_HTML = ["""<div id="musicWrap">
+    <table>
+        <tbody>
+            <td><a href="page?seqID=1">test-1</a></td>
+            <td><a href="page?seqID=2">test-2</a></td>
+            <td><a href="page?seqID=3">test-3</a></td>
+            <td><a href="page?seqID=4">test-4</a></td>
+            <td><a href="page?seqID=5">test-5</a></td>
+            <td><a href="page?seqID=6">test-6</a></td>
+            <td><a href="page?seqID=7">test-7</a></td>
+            <td><a href="page?seqID=8">test-8</a></td>
+            <td><a href="page?seqID=9">test-9</a></td>
+            <td><a href="page?seqID=10">test-10</a></td>
+        </tbody>
+    </table>
+</div>""", """<div id="musicWrap">
+    <table>
+        <tbody>
+            <td><a href="page?seqID=11">test-11</a></td>
+            <td><a href="page?seqID=12">test-12</a></td>
+            <td><a href="page?seqID=13">test-13</a></td>
+            <td><a href="page?seqID=14">test-14</a></td>
+            <td><a href="page?seqID=15">test-15</a></td>
+            <td><a href="page?seqID=16">test-16</a></td>
+            <td><a href="page?seqID=17">test-17</a></td>
+            <td><a href="page?seqID=18">test-18</a></td>
+            <td><a href="page?seqID=19">test-19</a></td>
+            <td><a href="page?seqID=20">test-20</a></td>
+        </tbody>
+    </table>
+</div>""", """<div id="musicWrap">
+    <table>
+        <tbody>
+            <td><a href="page?seqID=21">test-21</a></td>
+            <td><a href="page?seqID=22">test-22</a></td>
+            <td><a href="page?seqID=23">test-23</a></td>
+            <td><a href="page?seqID=24">test-24</a></td>
+            <td><a href="page?seqID=25">test-25</a></td>
+            <td><a href="page?seqID=26">test-26</a></td>
+            <td><a href="page?seqID=27">test-27</a></td>
+            <td><a href="page?seqID=28">test-28</a></td>
+        </tbody>
+    </table>
+</div>"""
+]
--- tests/musicCrawlerTest.py
+++ tests/musicCrawlerTest.py
@@ -1,15 +1,21 @@
+from datetime import date
 from unittest import TestCase
+from unittest.mock import patch, PropertyMock
+
+from mbcmusiccrawler.MBCMusicCrawler import MBCMusicCrawler, ProgramCode
+from tests.constants import ONE_MUSIC_LIST_ID_HTML, ONE_PAGE_MUSIC_LIST_ID_HTML, MANY_PAGE_MUSIC_LIST_ID_HTML
 
 
 class MusicCrawlerTest(TestCase):
-    def test_search_one_music_list_ids_by_date(self):
-        pass
+    def setUp(self) -> None:
+        self.music_crawler = MBCMusicCrawler()
 
-    def test_search_one_page_music_list_ids_by_date(self):
-        pass
+    @patch('requests.post', return_value=PropertyMock(text=ONE_MUSIC_LIST_ID_HTML))
+    def test_search_one_music_list_ids(self, mocked_requests):
+        result = self.music_crawler._search_music_list_ids_by_date(date.today(), ProgramCode.MUSIC_CAMP)
 
-    def test_search_many_page_music_list_ids_by_date(self):
-        pass
+        self.assertEqual(len(result), 1)
+        self.assertEqual(mocked_requests.call_count, 1)
 
     def test_parse_music_id_list_from_page(self):
         pass
